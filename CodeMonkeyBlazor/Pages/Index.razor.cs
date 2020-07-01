30367c3cbed3e6b78e0d5b1e6e40c6f4ebdebc81
haeng <ymca0147@naver.com>
2020-07-01 23:53:54 +0900

    Blazor 강의_JavaScript Interop을 사용하여 Bootstrap Carousel 컴포넌트 실행하도록 설정

--- CodeMonkeyBlazor/Pages/Index.razor.cs
+++ CodeMonkeyBlazor/Pages/Index.razor.cs
@@ -1,10 +1,22 @@
-﻿namespace CodeMonkeyBlazor.Pages
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace CodeMonkeyBlazor.Pages
 {
     public partial class Index
     {
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; }
+
         protected override void OnInitialized()
         {
-            base.OnInitialized();
+
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await JSRuntime.InvokeAsync<object>("RunCarousel");
         }
     }
 }
